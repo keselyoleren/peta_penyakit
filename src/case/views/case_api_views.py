072a3b81a7872f3ff4268bcea5ce6860d8926211
M Hadi Sasmito <keselyoleren@gmail.com>
2023-11-18 11:38:00 +0700

    add filter date

--- src/case/views/case_api_views.py
+++ src/case/views/case_api_views.py
@@ -1,9 +1,12 @@
+from time import time
 from rest_framework import (
     generics,
     status,
     viewsets,
     response
 )
+
+from django.utils import timezone
 
 from case.models import *
 from case.serialize.address_serialize import *
@@ -20,5 +23,26 @@ class CaseApiView(generics.ListAPIView):
     permission_classes = [AllowAny | IsAuthenticated]
     pagination_class = ResponsePagination
         
+    def get_queryset(self):
+        time_filter = self.request.query_params.get('time_filter', 'all')
+        if time_filter:
+            if time_filter == 'today':
+                start_date = timezone.now().date()
+                end_date = start_date + timezone.timedelta(days=1)
+            elif time_filter == 'this_week':
+                end_date = timezone.now().date()
+                start_date = end_date - timezone.timedelta(days=7)
+            elif time_filter == 'this_month':
+                end_date = timezone.now().date()
+                start_date = end_date - timezone.timedelta(days=30)
+            elif time_filter == 'this_year':
+                end_date = timezone.now().date()
+                start_date = end_date - timezone.timedelta(days=365)
+            else:
+                return super().get_queryset()        
+            return super().get_queryset().filter(created_at__range=[start_date, end_date])
+
+        return super().get_queryset()        
+
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)

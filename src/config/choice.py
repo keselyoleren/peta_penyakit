faca71ab618b2bc2090551d93c38e18fe2835a9f
M Hadi Sasmito <keselyoleren@gmail.com>
2023-12-02 10:36:51 +0700

    api import and progress

--- src/config/choice.py
+++ src/config/choice.py
@@ -4,3 +4,10 @@ class RoleUser(TextChoices):
     ADMIN = 'admin'
     PUSKESWAN = 'puskeswan'
     USER = 'user'
+
+
+class ThreadResult(TextChoices):
+    OK = 'OK'
+    ALREADY_RUN = 'ALREADY_RUN'
+    ERROR = 'ERROR'
+    INFO = 'INFO'
